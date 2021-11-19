ecad54c42157dd31c7875e173e24ae3b0ca2a447
Loki2140 <loki2140@gmail.com>
2021-11-20 00:38:26 +0200

    add common js

---
+++ slider.js
@@ -0,0 +1,14 @@
+class Slider {
+    constructor(img, nav, nav_points) {
+        this.img = img;
+        this.nav = nav;
+        this.nav_points = nav_points;
+    }
+
+    rightIMG(){
+
+    }
+    leftIMG(){
+        
+    }
+}
