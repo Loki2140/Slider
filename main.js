974ccfd3d6df4c04b54bf8e8e81fd96511fa69ac
Loki2140 <loki2140@gmail.com>
2021-11-21 18:28:50 +0200

    add lost main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+const imgages = document.querySelectorAll(".slider_item");
+const navButtons = document.querySelectorAll(".nav_buttons");
+const nav_points = document.querySelectorAll(".nav_points");
+
+
+const slider = new Slider(
+    imgages,
+    navButtons,
+    nav_points
+);
