d84c92bba33c6d264edac5758a2bd704f762485c
Loki2140 <loki2140@gmail.com>
2021-11-21 18:25:38 +0200

    main js logic's, add main.js / new css/ rewrite some html structure

--- slider.js
+++ slider.js
@@ -1,14 +1,56 @@
 class Slider {
-    constructor(img, nav, nav_points) {
+    constructor(img, navButtons, nav_points, position) {
+        this.position = 0;
         this.img = img;
-        this.nav = nav;
         this.nav_points = nav_points;
+        this.navButtons = navButtons;
     }
 
-    rightIMG(){
-
+    __setPossition(position) {
+        if (position >= this.img.lenght)
+            this.position = 0;
+        else if (position < 0)
+            this.position = this.img.lenght - 1;
+        else
+            this.position = position;
     }
-    leftIMG(){
-        
+
+    changeByPoints(position) {
+        if (position >= 0 && position < this.image.lenght) {
+            this.__removeActive(position);
+            this.__setPossition(position);
+            this.__addActive(position);
+        }
+    }
+    changeRightIMG() {
+        this.__removeActive(position);
+        this.__setPossition(this.index + 1);
+        this.__addActive(position);
+    }
+    changeLeftIMG() {
+        this.__removeActive(position);
+        this.__setPossition(this.index - 1);
+        this.__addActive(position);
+    }
+    __addActive(position) {
+        this.img[position].classList.add(activeItem);
+        this.nav_points[position].classList.add(activeButton);
+    }
+    __removeActive() {
+        this.img[position].classList.remove(activeItem);
+        this.nav_points[position].classList.remove(activeButton);
+    }
+
+    clickerListener(event) {
+        let target = event.target.dataset.target;
+        if (target) {
+            event.preventDefault();
+            if (target.toLowerCase() === "right") {
+                this.changeRightIMG();
+            } else if (target.toLowerCase() === "left") {
+                this.changeLeftIMG();
+            } else if (!isNaN(targen))
+                this.changeByPoints(Number.parseInt(target));
+        }
     }
 }
