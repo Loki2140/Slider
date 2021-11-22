df97c4b0e2d981199aa5b2bdea700b2f4145190a
Loki2140 <loki2140@gmail.com>
2021-11-23 00:30:38 +0200

    Finall JS class event listener

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
-const imgages = document.querySelectorAll(".slider_item");
-const navButtons = document.querySelectorAll(".nav_buttons");
-const nav_points = document.querySelectorAll(".nav_points");
-
-
-const slider = new Slider(
-    imgages,
-    navButtons,
-    nav_points
+const slider1 = new Slider(
+    '.slaid1'
+);
+const slider2 = new Slider(
+    '.slaid2'
 );
--- slider.js
+++ slider.js
@@ -1,55 +1,67 @@
 class Slider {
-    constructor(img, navButtons, nav_points, position) {
+
+    constructor(address) {
+        this.slideToShow = 1;
+        this.SlidetoScrolle = 1;
         this.position = 0;
-        this.img = img;
-        this.nav_points = nav_points;
-        this.navButtons = navButtons;
+        this.img = document.querySelector(address).querySelectorAll(".slider_item");
+        debugger;
+        this.nav_points = document.querySelector(address).querySelectorAll(".nav_points").querySelectorAll(buttons);
+        this.navButtons = document.querySelector(address).querySelectorAll(".nav_buttons");
+        this.slider = document.querySelector(address);
+        this.event = this.slider.addEventListener("click", this.__clickerListener.bind(this));
     }
+    // __setSlideToShow(){
+    //     item.forEach(element => {
+
+    //     });
+    //     this.img.css{
+
+    //     }
+    // }
 
     __setPossition(position) {
-        if (position >= this.img.lenght)
+        if (position >= this.img.length)
             this.position = 0;
         else if (position < 0)
-            this.position = this.img.lenght - 1;
+            this.position = this.img.length - 1;
         else
             this.position = position;
     }
 
-    changeByPoints(position) {
-        if (position >= 0 && position < this.image.lenght) {
-            this.__removeActive(position);
-            this.__setPossition(position);
-            this.__addActive(position);
-        }
+    changeByPoints(numbmer) {
+        this.__removeActive(this.position);
+        if (numbmer >= 0 && numbmer < this.img.length)
+            this.__setPossition(numbmer);
+        this.__addActive(this.position);
     }
     changeRightIMG() {
-        this.__removeActive(position);
-        this.__setPossition(this.index + 1);
-        this.__addActive(position);
+        this.__removeActive(this.position);
+        this.__setPossition(this.position + 1);
+        this.__addActive(this.position);
     }
     changeLeftIMG() {
-        this.__removeActive(position);
-        this.__setPossition(this.index - 1);
-        this.__addActive(position);
+        this.__removeActive(this.position);
+        this.__setPossition(this.position - 1);
+        this.__addActive(this.position);
     }
     __addActive(position) {
-        this.img[position].classList.add(activeItem);
-        this.nav_points[position].classList.add(activeButton);
+        this.img[position].classList.add('activeItem');
+        this.nav_points[position].classList.add('activeButton');
     }
-    __removeActive() {
-        this.img[position].classList.remove(activeItem);
-        this.nav_points[position].classList.remove(activeButton);
+    __removeActive(position) {
+        this.img[position].classList.remove('activeItem');
+        this.nav_points[position].classList.remove('activeButton');
     }
-
-    clickerListener(event) {
+    // Стоит ли данный метод делать приватным? разобраться... 
+    __clickerListener(event) {
         let target = event.target.dataset.target;
         if (target) {
-            event.preventDefault();
             if (target.toLowerCase() === "right") {
                 this.changeRightIMG();
             } else if (target.toLowerCase() === "left") {
                 this.changeLeftIMG();
-            } else if (!isNaN(targen))
+            } else if (!isNaN(target))
                 this.changeByPoints(Number.parseInt(target));
         }
     }
