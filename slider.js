5c16a69ba7df574bbfe7b6ac47452b5ae468e812
Loki2140 <93878720+Loki2140@users.noreply.github.com>
2021-11-23 13:15:31 +0200

    Several fixes

--- slider.js
+++ slider.js
@@ -1,16 +1,17 @@
 class Slider {
 
     constructor(address) {
+        this.slider = document.querySelector(address);
         this.slideToShow = 1;
         this.SlidetoScrolle = 1;
+        this.scrollable = true;
         this.position = 0;
         this.img = document.querySelector(address).querySelectorAll(".slider_item");
-        debugger;
         this.nav_points = document.querySelector(address).querySelectorAll("button");
         this.navButtons = document.querySelector(address).querySelectorAll(".nav_buttons");
-        this.slider = document.querySelector(address);
-        this.event = this.slider.addEventListener("click", this.__clickerListener.bind(this));
+        this.event = this.slider.addEventListener("click", this._clickerListener.bind(this));
     }
+
     // __setSlideToShow(){
     //     item.forEach(element => {
 
@@ -19,42 +20,16 @@ class Slider {
 
     //     }
     // }
-
-    __setPossition(position) {
-        if (position >= this.img.length)
-            this.position = 0;
-        else if (position < 0)
-            this.position = this.img.length - 1;
-        else
-            this.position = position;
-    }
-
-    changeByPoints(numbmer) {
-        this.__removeActive(this.position);
-        if (numbmer >= 0 && numbmer < this.img.length)
-            this.__setPossition(numbmer);
-        this.__addActive(this.position);
-    }
-    changeRightIMG() {
-        this.__removeActive(this.position);
-        this.__setPossition(this.position + 1);
-        this.__addActive(this.position);
-    }
-    changeLeftIMG() {
-        this.__removeActive(this.position);
-        this.__setPossition(this.position - 1);
-        this.__addActive(this.position);
-    }
-    __addActive(position) {
+    _addActive(position) {
         this.img[position].classList.add('activeItem');
         this.nav_points[position].classList.add('activeButton');
     }
-    __removeActive(position) {
+    _removeActive(position) {
         this.img[position].classList.remove('activeItem');
         this.nav_points[position].classList.remove('activeButton');
     }
     // Стоит ли данный метод делать приватным? разобраться... 
-    __clickerListener(event) {
+    _clickerListener(event) {
         let target = event.target.dataset.target;
         if (target) {
             if (target.toLowerCase() === "right") {
@@ -65,4 +40,30 @@ class Slider {
                 this.changeByPoints(Number.parseInt(target));
         }
     }
+    _setPossition(position) {
+        if (position >= this.img.length)
+            this.position = 0;
+        else if (position < 0)
+            this.position = this.img.length - 1;
+        else
+            this.position = position;
+    }
+
+    changeByPoints(numbmer) {
+        this._removeActive(this.position);
+        if (numbmer >= 0 && numbmer < this.img.length)
+            this._setPossition(numbmer);
+        this._addActive(this.position);
+    }
+    changeRightIMG() {
+        this._removeActive(this.position);
+        this._setPossition(this.position + 1);
+        this._addActive(this.position);
+    }
+    changeLeftIMG() {
+        this._removeActive(this.position);
+        this._setPossition(this.position - 1);
+        this._addActive(this.position);
+    }
+
 }
