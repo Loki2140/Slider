6fe9b6ae0c9d6d7446e250494dc56ef1d2fb3893
Loki2140 <loki2140@gmail.com>
2021-12-13 01:15:08 +0200

    Final, ver 1.0

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ const slider1 = new Slider(
     '.slaid1', {
         SlideToShow: 2,
         SlidetoScrolle: 2,
+        Scrollable: false
+    }
+);
+const slider2 = new Slider(
+    '.slaid2', {
+        SlideToShow: 2,
+        SlidetoScrolle: 1,
         Scrollable: true
     }
 );
-// const slider2 = new Slider(
-//     '.slaid2', {
-//         slideToShow: 1,
-//         SlidetoScrolle: 1,
-//         scrollable: true
-//     }
-// );
--- slider.js
+++ slider.js
@@ -1,59 +1,35 @@
 "use strict";
 class Slider {
     constructor(address, settings) {
-        // по сути settings можно было передавать на прямую в конструктор, 
-        // но захотел заморочится и сделать это через объект, как например в SlickSlider
+        // Иницилизация слайденра
         this.slider = document.querySelector(address);
         this.settings = settings;
-        this.position = 0;
+        // Сетинги по умолчанию, до их иницилизаии - по сути settings можно было передавать на прямую в конструктор, но захотел заморочится и сделать это через объект, как например в SlickSlider
         this.imgWidth = this.slider.clientWidth / 1;
         this.slidesToScroll = 0;
         this.slidesToShow = 1;
         this.scrollable = false;
+        // this.odd = 0;
+        this.width;
+        // Начальные позиции 
+        this.lastPosition;
+        this.positionButtons = 0;
+        this.curentPosition = 0;
+        // Иницилизация элементов слайдера 
         this.items = this.slider.querySelector(".slider_items");
         this.img = this.slider.querySelectorAll(".slider_item");
-        this.nav_points = this.slider.querySelectorAll("button");
+        this.nav_point = this.slider.querySelector(".points");
+        // Загрузка установленных Сетингов
+        this.slider.addEventListener("onload", this._settingsReader(settings));
+        // Иницилизация элементов навигации слайдера 
+        this.nav_points = this.slider.querySelectorAll(".nav_points");
         this.navButtons = this.slider.querySelectorAll(".nav_buttons");
-        document.addEventListener('onload', this._settingsReader(settings));
-        this.event = this.slider.addEventListener("click", this._clickerListener.bind(this)); // Почему тут пришлось биндить а выше нет, не совсем понимаю контекст. 
+        // Ивенты кнопки -Почему тут пришлось биндить а выше ивент нет, не совсем понимаю контекст. 
+        this.slider.addEventListener("onload", this._settingsInit());
+        this.slider.addEventListener("click", this._clickerListener.bind(this));
+        window.addEventListener("resize", () => this._settingsInit()); // не могу понять почему работает именно вот так '() =>' работает 
     }
-
-
-    _setSlideToShow(settings) {
-        if (settings > this.img.length && settings <= 0) // Проверка на дурака
-            settings = 1;
-        this.slidesToShow = settings;
-        this.imgWidth = this.slider.clientWidth / settings;
-        this.img.forEach((item) => {
-            item.style.minWidth = `${this.imgWidth}px`
-        });
-    }
-    _setSlideToScrolle(settings) {
-        if (settings > this.img.length) {
-            settings = 1;
-        } else if (settings === this.img.length) {
-            this.slidesToScroll = 0;
-        } else {
-            this.slidesToScroll = settings;
-        }
-    }
-
-    _setScrollable(settings) {
-        if (settings == true)
-            this.scrollable = true;
-    }
-
-    // _setPoints() {
-    //     debugger;
-    //     const create = this.nav_points.createElement('ul');
-
-    //     // create.innerHTML = this.img.forEach((item) => {
-    //             // '<li class="nav_points"><button class="activeButton" type="button" data-target="' + item + '" ></button></li>'
-    //         // }
-    //         // this.nav_points.appendChild(create);
-    //     }
-
-
+    // Методы сетингов 
 
     _settingsReader(settings) {
         for (const key in settings) {
@@ -67,23 +43,48 @@ class Slider {
                 }
             }
         }
-        // this._setPoints();
-    } //в теории можно сломать поменяв параметры местами, надо проверить 
+        this._setPoints();
+    }
+    _setSlideToShow(settings) {
+        if (settings > this.img.length && settings <= 0) // Проверка на дурака
+            settings = 1;
+        this.slidesToShow = settings;
+    }
+    _setSlideToScrolle(settings) {
+        if (settings > this.img.length) {
+            settings = 1;
+        } else if (settings === this.img.length) {
+            this.slidesToScroll = 0;
+        } else {
+            this.slidesToScroll = settings;
+        }
+    }
+    _setScrollable(settings) {
+        if (settings == true)
+            this.scrollable = true;
+    }
 
+    // Просчитываем и выставляем кол навигационных маркеров 
+    _setPoints() {
+        const ul = document.createElement('ul'),
+            points = Math.ceil((this.img.length - this.slidesToShow) / this.slidesToScroll + 1);
+        // this.odd = ((this.img.length - this.slidesToShow) / this.slidesToScroll + 1) - Math.floor((this.img.length - this.slidesToShow) / this.slidesToScroll + 1);
+        // console.log(this.odd); - удалить
+        for (let key = 0; key < points; key++) {
+            key == 0 ? ul.insertAdjacentHTML("beforeend", `<li><button class='nav_points activeButton' type="button" data-target="` + key + `"></button></li>`) : ul.insertAdjacentHTML("beforeend", `<li><button class="nav_points" type="button" data-target="` + key + `"></button></li>`);
+        }
+        this.nav_point.append(ul);
+    }
+
+    // Методы Цсс стилей
     _addActive(position) {
-        debugger;
-        console.log(this.position);
-        this.img[position].classList.add('activeItem');
         this.nav_points[position].classList.add('activeButton');
     }
-
     _removeActive(position) {
-        console.log(this.position);
-        debugger;
-        this.img[position].classList.remove('activeItem');
         this.nav_points[position].classList.remove('activeButton');
     }
-
+    // Работа с навигацией 
+    // Ивенты 
     _clickerListener(event) {
         let target = event.target.dataset.target;
         if (target) {
@@ -95,68 +96,108 @@ class Slider {
                 this.changeByPoints(Number.parseInt(target)); // на всякий случай
         }
     }
+    _settingsInit() {
+        console.log('resize')
+        let width = document.querySelector('.slider_container').offsetWidth;
+        this.imgWidth = width / this.slidesToShow; // в чем разница elem.offsetWidth и elem.clientWidth
+        this.items.style.width = this.imgWidth * this.img.length + "px";
+        this.img.forEach((item) => {
+            item.style.width = `${this.imgWidth}px`;
+            item.style.hight = "auto";
+            console.log(item.style.width);
+        })
+        this._movePoints(this.positionButtons);
+        if (this.scrollable === false)
+            this._chkNavButtons();
+    }
+    // Основной подсчет
     _setPossition(position) {
-        debugger;
         if (this.scrollable == true) {
-            if (position >= this.img.length) {
-                this.position = 0;
+            if (position > this.nav_points.length - 1) {
+                this.positionButtons = 0;
             } else if (position < 0)
-                this.position = this.img.length - 1;
-            else
-                this.position = position;
+                this.positionButtons = this.nav_points.length - 1;
+            else {
+                this.positionButtons = position;
+            }
         } else {
-            if (position >= this.img.length) {
-                this.position = this.img.length - 1;
+            if (position >= this.nav_points.length - 1) {
+                this.positionButtons = this.nav_points.length - 1;
             } else if (position < 0) {
-                this.position = 0
+                this.positionButtons = 0;
             } else {
-                this.position = position;
+                this.positionButtons = position;
+            }
+            this._chkNavButtons();
+        }
+    }
+    // Проверка Кнопок Влево, Вправо
+    _chkNavButtons(d) {
+        this.slider.querySelector('.left').disabled = this.positionButtons == 0;
+        this.slider.querySelector('.right').disabled = this.positionButtons == this.nav_points.length-1;
+    }
+
+    // Движение нижние навигационные маркеры И ОСНОВНОЙ навигации - на базе этого метода можно заменить метод _move. 
+    _movePoints(numbmer) {
+        this._setPossition(numbmer);
+        if (this.positionButtons == 0) {
+            this.curentPosition = 0
+        } else if (this.positionButtons == this.nav_points.length - 1) {
+            this.curentPosition = ((this.img.length - this.slidesToShow) * this.imgWidth) * -1;
+        } else {
+            this.curentPosition = (numbmer * (this.slidesToScroll * this.imgWidth)) * -1
+        }
+        this.items.style.transform = `translateX(${this.curentPosition}px)`; // можно сократить в новый метод
+    }
+
+    // Движение основной навигации. - Можно значительлно сократить. ВЫКЛЮЧЕННО \||/
+
+    _move(progress) {
+        if (progress == undefined)
+            progress = -1;
+        const movePos = this.slidesToScroll * this.imgWidth,
+            itemCount = this.img.length;
+        if (progress == -1) {
+            // next
+            const ImgLeft = itemCount - (Math.abs(this.curentPosition) + this.slidesToShow * this.imgWidth) / this.imgWidth;
+            this._setPossition(this.positionButtons - progress);
+            if (this.scrollable == true && ImgLeft == 0) {
+                this.curentPosition = 0;
+            } else {
+                this.curentPosition -= ImgLeft >= this.slidesToScroll ? movePos : ImgLeft * this.imgWidth; // можно сократить в новый метод
+            }
+        } else {
+            // prev
+            const ImgLeft = Math.abs(this.curentPosition) / this.imgWidth;
+            this._setPossition(this.positionButtons - progress);
+            if (this.scrollable == true && ImgLeft == 0) {
+                this.curentPosition = -(itemCount - this.slidesToShow) * this.imgWidth;
+            } else {
+                this.curentPosition += ImgLeft >= this.slidesToScroll ? movePos : ImgLeft * this.imgWidth; // можно сократить в новый метод
             }
         }
-        console.log(this.position);
+        this.items.style.transform = `translateX(${this.curentPosition}px)`; // можно сократить в новый метод
     }
+    // ВЫКЛЮЧЕННО /||\
 
-    _chkNavButtons() {
-
-    }
-    _move(position) {
-        const imgLeft = this.img.length - position;
-        if (imgLeft >= this.slidesToScroll) {
-            this.items.style.transform = `translateX(${-position*this.imgWidth}px)`;
-        } else {
-            this.items.style.transform = `translateX(${-(position-(this.slidesToScroll-imgLeft))*this.imgWidth}px)`;
-            this.position = position + imgLeft;
-        }
-        console.log(this.position);
-    }
-
-    changeByPoints(numbmer) {
-        this._removeActive(this.position);
-        if (numbmer >= 0 && numbmer <= this.img.length) //Проверка на дурака 
-            this._setPossition(numbmer);
-        this._move(numbmer-1);
-        this._addActive(numbmer-1);
-    }
+    // Основная навигация 
     changeRightIMG() {
-        // this._removeActive(this.position);
-        this._setPossition(this.position + this.slidesToScroll);
-        this._move(this.position);
-        // this._addActive(this.position);
+        this._removeActive(this.positionButtons);
+        this._movePoints(this.positionButtons + 1)
+        // this._move(-1); // ВЫКЛЮЧЕННО
+        this._addActive(this.positionButtons);
     }
     changeLeftIMG() {
-        // this._removeActive(this.position);
-        this._setPossition(this.position - this.slidesToScroll);
-        this._move(this.position);
-        // this._addActive(this.position);
+        this._removeActive(this.positionButtons);
+        this._movePoints(this.positionButtons - 1)
+        // this._move(+1); // ВЫКЛЮЧЕННО
+        this._addActive(this.positionButtons);
+    }
+    // Навигационные маркеры 
+    changeByPoints(numbmer) {
+        this._removeActive(this.positionButtons);
+        if (numbmer >= 0 && numbmer <= this.nav_points.length) //Проверка на дурака 
+            this._movePoints(numbmer, -1);
+        this._addActive(this.positionButtons);
     }
 }
-
-
-//     debugger;
-// const imgLeft = this.img.length - (this.position * this.imgWidth + this.slidesToScroll * this.imgWidth) / this.imgWidth;
-// console.log(`осталось ${imgLeft}`);
-// this.positionPX -= imgLeft * this.imgWidth >= this.slidesToScroll ? this.slidesToScroll * this.imgWidth : imgLeft * this.imgWidth;
-// this.items.style.transform = `translateX(${this.positionPX}px)`;
-// const positionPX = imgLeft >= this.slidesToScroll ? this.slidesToScroll * this.imgWidth : imgLeft * this.imgWidth
-// positionPX=(this.position-(this.slidesToScroll-imgLeft))*this.imgWidth;
-// this.items.style.transform = `translateX(${-this.position*this.imgWidth+positionPX}px)`;
